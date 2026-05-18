3671407337c6e803f3a3be034a6d40121ddce2fe
Ian Wilson <ian@gardentheory.com>
2026-05-18 15:45:09 -0700

    Use component.

--- src/flask_tdom_poc/__init__.py
+++ src/flask_tdom_poc/__init__.py
@@ -1,3 +1,5 @@
+from string.templatelib import Template
+
 from flask import Flask
 from flask_tdom import render_template
 
@@ -5,6 +7,17 @@ from flask_tdom import render_template
 app = Flask(__name__)
 
 
+def Layout(children: Template) -> Template:
+    return t"""<!doctype html>
+<html lang="en-US">
+<head><meta charset="utf8"></head>
+<body>
+{children}
+</body>
+</html>
+"""
+
+
 @app.route("/")
 def hello_world():
-    return render_template(t"<p>Hello, World!</p>")
+    return render_template(t"<{Layout}><p>Hello, World!</p></{Layout}>")
