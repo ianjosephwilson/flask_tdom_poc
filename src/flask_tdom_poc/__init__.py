47f248df555d3c0275170264ca0e03b2eb23eee0
Ian Wilson <ian@gardentheory.com>
2026-05-24 18:01:02 -0700

    Actually demo use of context.

--- src/flask_tdom_poc/__init__.py
+++ src/flask_tdom_poc/__init__.py
@@ -1,17 +1,20 @@
 from string.templatelib import Template
 
 from flask import Flask
-from flask_tdom import render_template
+from flask_tdom import render_template, FlaskCtx
 
 
 app = Flask(__name__)
 
 
 def Layout(children: Template) -> Template:
+    flask_info = FlaskCtx.get()
     return t"""<!doctype html>
 <html lang="en-US">
 <head><meta charset="utf8"></head>
 <body>
+<!-- check that flask template context is set -->
+{flask_info['request'].host_url}
 {children}
 </body>
 </html>
